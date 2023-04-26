6afb3f121620c86269985bc8ed77f20c8b8d1797
Addit <adam.garza33@hotmail.com>
2023-04-25 23:52:54 -0500

    added to index.js by adding relationship w/ Tags and Products

--- models/index.js
+++ models/index.js
@@ -20,6 +20,12 @@ Product.belongsToMany(Tag, {
   foreignKey: 'product_id'
 })
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  // as: 'product_tag',
+  foreignKey: 'tag_id'
+})
+
 
 module.exports = {
   Product,
