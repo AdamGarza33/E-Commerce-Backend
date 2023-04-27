e57ef31fe94ef840522912a55da1c0a9983e1adc
Addit <adam.garza33@hotmail.com>
2023-04-26 19:00:59 -0500

    added findall to find single tag w/ product data and find one to find a single tag by id w associtated product data

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,12 +5,47 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
+  Tag.findAll({
   // be sure to include its associated Product data
+  include: [
+    {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+    }
+  ]
+})
+  .then(dbTagData => res.json(dbTagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
+  Tag.findOne({
   // be sure to include its associated Product data
+  where: {
+    id: req.params.id
+  },
+  include: [
+    {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+    }
+  ]
+})
+  .then(dbTagData => {
+    if (!dbTagData) {
+      res.status(404).json({ message: 'No tag found with this id'});
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
